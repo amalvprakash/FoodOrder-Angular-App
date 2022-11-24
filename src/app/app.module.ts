6e40b2e0f597b752be7644fc2050cab29ca29356
Amal <amalprakash2025@gmail.com>
2022-11-25 00:43:48 +0530

    added login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 const myRoute:Routes=[
   {
     path:"",
     component:HomeComponent
+  },
+  {
+    path:"login",
+    component:LoginComponent
   }
 ]
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
