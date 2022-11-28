5f1fc47f66f6568b933cfcd07bc9256b8b0908b0
Amal <amalprakash2025@gmail.com>
2022-11-28 23:58:42 +0530

    added binding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username = ""
+  password = ""
 
+
+  login = () =>{
+    let data:any = {
+    "username":this.username,
+    "password":this.password
+    
+    }
+    console.log(data)
+
+    if (this.username=="admin" && this.password == "12345") {
+      alert("login successfull")
+    } else {
+      alert("login failed")
+
+    }
+  }
 }
--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./order.component.css']
 })
 export class OrderComponent {
+  name = ""
+  address = ""
+  pincode = ""
+  phone = ""
+  email = ""
+  landmark = ""
+  orderInstruction = ""
 
+  order = () =>{
+    let data:any = {
+    "name":this.name,
+    "address":this.address,
+    "pincode":this.pincode,
+    "email":this.email,
+    "phone":this.phone,
+    "landmark":this.landmark,
+    "orderInstruction":this.orderInstruction
+    }
+    console.log(data)
+  }
 }
